c5834e4d4918cf0b71aa6604cc76fd40d4e21c9b
Damon Burda <damonburda96@gmail.com>
2021-05-05 21:11:06 -0700

    updated model with password validation

--- bandstand/models/User.js
+++ bandstand/models/User.js
@@ -3,9 +3,20 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema ({
     firstName:  String,
+    
     lastName: String,
-    username: { type: String, required: true, index: {unique: true} },
-    body:  { type: String, required: true }
+
+    username: { 
+        type: String,
+         required: true, 
+         index: {unique: true} 
+    },
+
+    pasword:  { 
+        type: String, 
+        required: true,
+        validate: [({ length }) => length >= 6, "Password should be longer."] 
+    }
 });
 
 const User = mongoose.model("Post", userSchema);
