038189bddaf5b98820f485511a1fa871c6671c8f
Damon Burda <damonburda96@gmail.com>
2021-05-23 16:12:20 -0700

    state, props, hooks login

--- bandstand/client/src/pages/Login.js
+++ bandstand/client/src/pages/Login.js
@@ -6,7 +6,6 @@ import Error from "../components/elements/Error"
 import "./log.css"
 import {
     COMMON_FIELDS,
-    REGISTRATION_FIELDS,
     LOGIN_FIELDS,
     LOGIN_MESSAGE,
     ERROR_IN_LOGIN,
@@ -54,23 +53,40 @@ export default class Login extends Component {
             error: false,
           });
     };
-    
+
     render() {
+        const { loginSuccess, error } = this.state;
+
         return (
-          <div className="log">
-            <form>
-                <h3>Sign In</h3>
+          <div className="Login">
+            <form onSubmit={this.onSubmit}>
+                <h3>{LOGIN_FIELDS.LOGIN_HEADING}</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
+                    <label> {COMMON_FIELDS.USER_NAME} </label>
+                    <input 
+                    type="TEXT" 
+                    name="Username" 
+                    onChange={this.handleOnChangeUserName}
+                    autoComplete= "Username"
+                    required
+                    placeholder="Enter username" 
+                    />
+                </div>{' '}
+                {' '}
                 <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
+                    <label>{COMMON_FIELDS.PASSWORD}</label>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    onChange={this.handleOnChangePassword}
+                    autoComplete="Password"
+                    required
+                    placeholder="Enter password" 
+                    />{' '}
+                        {' '}
+                </div>{' '}
+                {' '}
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
@@ -78,17 +94,25 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <Link to="/chatroom"><button type="submit" className="btn btn-primary btn-block">
+                <div className="buttons">
+                    {' '}
+                <Link to="/chatroom">
+                <button 
+                type="button"
+                onClick={this.onSubmit} 
+                className="btn btn-primary btn-block">
                   Submit
-                  </button></Link>
-
+                  </button></Link>{' '}
+                </div>{' '}
 
                 <p className="forgot-password text-right">
                     <a href="/SignUp">Register An Account</a><br></br>
                     <a href="/forgotPass">Forgot My Password</a>
-                </p>
-            </form>
-          </div>
+                </p>{' '}
+            </form>{' '}
+        {loginSuccess && <Message message={LOGIN_MESSAGE} />}    {' '}
+        {error && <Error message={ERROR_IN_LOGIN} />}    {' '}
+        </div>
         );
     }
 }
